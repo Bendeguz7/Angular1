6bb271b1c22f2fa75dc1cfde3c3d74ff4c6feaed
Bendeguz7 <szs7@t-online.hu>
2014-11-18 11:24:57 +0100

    Ebbe irom, hogy mire jutottam az etalonnal
    Signed-off-by: Bendeguz7 <szs7@t-online.hu>

---
+++ discoverEtalon.js
@@ -0,0 +1,39 @@
+/* pg
+var pg = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    bindEvents: function() {
+
+      // bind deviceready callback for phonegap devices
+      document.addEventListener('deviceready', this.onDeviceReady, true);
+      //document.addEventListener("offline", function() { alert('Your offline')}, false);
+
+    },
+    // deviceready Event Handler
+    onDeviceReady: function() {
+        pg.receivedEvent('deviceready');
+
+        if (device.platform == 'iOS') {
+            $('body').addClass('platformIOS');
+        }
+        
+        setTimeout(function() {
+            navigator.splashscreen.hide();
+            console.log('dcs:: splashscreen faded after 1 seconds');
+        }, 1000);
+    
+        var _this = this;
+
+        angular.element(document).ready(function(){
+            angular.bootstrap(document, ['imApp']);
+            document.body.setAttribute('ng-app', 'imApp');
+        });
+
+        pushNotification = window.plugins.pushNotification;
+
+    },
+    // Update DOM on a Received Event
+*/
